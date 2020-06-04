e6c291415cf682be4d82ebcd240789ffa6525432
NganDo2911 <dothithanhngan2911@gmail.com>
2020-06-04 23:40:52 +0700

    complete bfs

--- FinalProject/FinalProject.cpp
+++ FinalProject/FinalProject.cpp
@@ -16,8 +16,6 @@ typedef struct node {
     int vertex;
     int depth;
     int path_cost;
-    //int parent;
-
 }Node;
 class Graph {
 public:
@@ -29,42 +27,83 @@ public:
         }
     }
 };
-bool bfs(Graph const& g, int src, int dest, vector<bool>& visited, vector<int>& path) {
-    queue<Node> q;
+void printPath(vector<vector<Edge>> adjList)
+{
+    for (int i = 0; i < adjList.size(); i++) {
+        cout << i << endl;
+        for (int j = 0; j < adjList[i].size(); j++) {
+            cout << "->" << adjList[i][j].end << " ";
+        }
+        cout << endl;
+    }
+}
+bool bfs(Graph const& g, int src, int dest, vector<bool> visited, int* pred, int n) {
+    queue<Node*> q;
 
-    int minCost = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        visited[i] = false;
+        pred[i] = -1;
+    }
+
     visited[src] = true;
-
-    q.push({ src,0,0 });
+    Node* current = (Node*)malloc(sizeof(Node));
+    current->vertex = src;
+    current->depth = 0;
+    current->path_cost = 0;
+    q.push(current);
 
     while (!q.empty()) {
-        Node node = q.front();
+        Node* node = q.front();
         q.pop();
 
-        int v = node.vertex;
-        int depth = node.depth;
-        int cost = node.path_cost;
-        path.push_back(v);
-        if (v == dest) {
-            return true;
-        }
-        for (Edge edge : g.adjList[v]) {
-            if (!visited[edge.end]) {
-                visited[edge.end] = true;
-                q.push({ edge.end,depth + 1,cost + edge.distance });
+        int v = node->vertex;
+        int depth = node->depth;
+        int cost = node->path_cost;
+        for (int i = 0; i < g.adjList[v].size(); i++) {
+            Node* newnode = (Node*)malloc(sizeof(Node));
+            if (visited[g.adjList[v][i].end] == false) {
+                visited[g.adjList[v][i].end] = true;
+                newnode->vertex = g.adjList[v][i].end;
+                newnode->depth = depth + 1;
+                newnode->path_cost = g.adjList[v][i].distance;
+                pred[g.adjList[v][i].end] = v;
+                q.push(newnode);
 
+                if (g.adjList[v][i].end == dest)
+                    return true;
             }
         }
     }
     return false;
 }
 
-void printPath(const vector<int>& path)
-{
-    for (int i : path)
-        cout << i << ' ';
-    cout << endl;
+void printShortestDistance(Graph const& g, int src, int dest, vector<bool> visited, int n) {
+    int* pred = new int[n];
+    vector<int> path(n);
+
+    if (bfs(g, src, dest, visited, pred, n) == false) {
+        cout << "Given source and destination"
+            << " are not connected";
+        return;
+    }
+    int crawl = dest;
+    path.push_back(crawl);
+    while (pred[crawl] != -1) {
+        path.push_back(pred[crawl]);
+        crawl = pred[crawl];
+    }
+
+    // printing path from source to destination 
+    cout << "\nPath is::\n";
+    for (int i = path.size() - 1; i >= 0; i--) {
+        cout << path[i] << " ";
+        if (path[i] == dest)
+            return;
+    }
+
 }
+
+
 
 //void printPath(Node* node) {
 //    if (node->parent == NULL)
@@ -99,6 +138,15 @@ int main()
     g.addEdge(16, 17, 92);
     g.addEdge(17, 18, 87);
     g.addEdge(19, 20, 86);*/
+    /*vector<Edge> edges =
+    {
+        {0,1,71},{0,3,151},{1,2,75},{2,3,140},
+        {2,4,118}, {3,11,99},{3,9,80},{4,5,111},
+        {5,6,70},{6,7,75},{7,8,120},{8,9,146},
+        {8,10,138},{9,10,97},{10,12,101},{11,12,211},
+        {12,13,90},{12,14,85},{14,18,98},{14,15,142},
+        {15,16,92},{16,17,87},{18,19,86}
+    };*/
     vector<Edge> edges =
     {
         {0,1,71},{0,3,151},{1,2,75},{2,3,140},
@@ -112,16 +160,23 @@ int main()
     Graph g(edges, N);
     int src = 0, dest = 19;
     vector<bool> visited(N);
-    vector<int> path;
+    int* path = new int[N];
 
-    if (bfs(g, src, dest, visited, path)) {
-        cout << "Path exists from vertex " << src << " to vertex " << dest;
-        cout << "\nThe complete path is: "; printPath(path);
+
+    printPath(g.adjList);
+    /*  if (bfs(g, src, dest, visited, path)) {
+          cout << "Path exists from vertex " << src << " to vertex " << dest;
+          cout << "\nThe complete path is: ";
+      }
+      else {
+          cout << "No path exists between vertices " << src << " and " << dest;
+      }*/
+    if (bfs(g, src, dest, visited, path, N) == false) {
+        cout << "False";
     }
     else {
-        cout << "No path exists between vertices " << src << " and " << dest;
+        printShortestDistance(g, src, dest, visited, N);
+        cout << "True";
     }
-
-
     return 0;
 }
